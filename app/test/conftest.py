35e18eee54a3b32f18b1f42730fb2544720c37a1
Kennedy Bacelar <kennedybacelar@yahoo.com.br>
2024-06-12 01:29:41 -0300

    adjusting formatting and moving import

--- app/test/conftest.py
+++ app/test/conftest.py
@@ -2,18 +2,16 @@ import os
 
 import pytest
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from app.main.database import init_db
-
-Base = declarative_base()
 TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL", "postgresql://username:password@localhost:5433/qorpo_database_test")
 
 
 @pytest.fixture
 def setup_test_database(monkeypatch):
     monkeypatch.setenv("DATABASE_URL", TEST_DATABASE_URL)
+    from app.main.database import init_db
+
     init_db(connect_only=False)
 
 
